9e89043d35cf194517e5db1ec8bb5498a06bdbfa
Kiran Niranjan <kiran@kike.co.in>
2017-05-28 15:46:38 +0530

    1. Updated gulp 2. Added License

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,64 @@
-var gulp = require("gulp");
-var browserify = require("browserify");
-var source = require('vinyl-source-stream');
-var watchify = require("watchify");
-var tsify = require("tsify");
-var gutil = require("gulp-util");
-var paths = {
+const gulp = require('gulp');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const ts = require('gulp-typescript');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const buffer = require('vinyl-buffer');
+
+const project = ts.createProject('tsconfig.json');
+const paths = {
     pages: ['src/*.html']
 };
 
-var watchedBrowserify = watchify(browserify({
-    basedir: '.',
-    debug: true,
-    entries: ['src/app.ts', 'src/main.ts'],
-    cache: {},
-    packageCache: {}
-}).plugin(tsify));
-
-gulp.task("copy-html", function () {
+// Gulp task for HTML
+gulp.task('build:html', () => {
     return gulp.src(paths.pages)
-        .pipe(gulp.dest("dist"));
+        .pipe(gulp.dest('dist'));
 });
 
-function bundle() {
-    return watchedBrowserify
-        .bundle()
-        .pipe(source('bundle.js'))
-        .pipe(gulp.dest("dist"));
-}
+gulp.task('watch:html', () => {
+    gulp.watch(paths.pages, ['build:html']);
+});
 
-gulp.task("default", ["copy-html"], bundle);
-watchedBrowserify.on("update", bundle);
-watchedBrowserify.on("log", gutil.log);
+// Gulp task for ts
+gulp.task('build:js', () => {
+    return project.src()
+        .pipe(project())
+        .js.pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch:js', () => {
+    gulp.watch([
+        'main.ts',
+        'src/**/*.ts',
+        'src/**/*.jsx'
+    ], ['build:js']);
+});
+
+// Gulp for node modules
+gulp.task('build:vendor', function() {
+    browserify({entries: ['./src/vendor.js']})
+        .bundle()
+        .pipe(source('vendor.min.js'))
+        .pipe(buffer())
+        .pipe(uglify({ mangle: false }))
+        .pipe(gulp.dest('dist'));
+});
+
+// Gulp for scss
+gulp.task('build:css', () => {
+    return gulp.src('src/styles/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/styles'));
+});
+
+gulp.task('watch:css', () => {
+    gulp.watch('src/**/*.scss', ['build:css']);
+});
+
+gulp.task('build', ['build:html', 'build:js', 'build:css', 'build:vendor']);
+gulp.task('watch', ['watch:html', 'watch:js', 'watch:css']);
+
+gulp.task('default', ['build', 'watch']);
